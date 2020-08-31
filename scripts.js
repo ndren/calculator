ece8613518621395e0826c56cd85fa87cd377cf9
Andrei E <andreien@protonmail.com>
2020-08-31 21:05:09 +0100

    Implement calculator functionality.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function add(a, b) {
-    return a + b;
+    return +a + +b;
 }
 function subtract(a, b) {
     return a - b;
@@ -11,6 +11,7 @@ function divide(a, b) {
     return a / b;
 }
 function operate(operator, a, b) {
+    if (!a) return b;
     switch (operator) {
         case '+':
             return add(a, b);
@@ -20,5 +21,70 @@ function operate(operator, a, b) {
             return multiply(a, b);
         case '/':
             return divide(a, b);
+        default:
+            return b;
     }
 }
+function clearDisplay() {
+    display.textContent = '';
+}
+function appendDigit() {
+    if (displayToBeCleared) {
+        clearDisplay();
+        displayToBeCleared = false;
+    }
+    if (display.textContent.length <= 15) {
+        display.textContent += this.textContent;
+    }
+}
+function appendPoint() {
+    if (!display.textContent.includes('e') && !display.textContent.includes('.')) {
+        display.textContent += '.';
+    }
+}
+function appendResult() {
+    display.textContent = newNum;
+}
+function returnResult() {
+    if (selectedOperator !== '/' || newNum !== '0') {
+        newNum = operate(selectedOperator, oldNum, newNum);
+        appendResult();
+    }
+    else {
+        display.textContent = 'Not on my watch - I mean display!'
+    }
+}
+function resetAll() {
+    display.textContent = ''
+    newNum = null;
+    oldNum = null;
+    selectedOperator = null;
+}
+function storeEquation() {
+    oldNum = newNum;
+    newNum = display.textContent;
+    clearDisplay();
+    returnResult();
+    if (this !== equalSign) { selectedOperator = this.textContent; }
+    displayToBeCleared = true;
+}
+let newNum;
+let oldNum;
+let selectedOperator;
+displayToBeCleared = false;
+let display = document.querySelector('#display');
+let operands = document.querySelectorAll('.operand');
+let operators = document.querySelectorAll('.operator');
+let equalSign = document.querySelector('#equalSign');
+let clear = document.querySelector('#clear');
+let decimalPoint = document.querySelector('#decimalPoint')
+
+operands.forEach(operand => {
+    operand.addEventListener('click', appendDigit);
+})
+operators.forEach(operator => {
+    operator.addEventListener('click', storeEquation);
+})
+equalSign.addEventListener('click', storeEquation);
+clear.addEventListener('click', resetAll);
+decimalPoint.addEventListener('click', appendPoint);
